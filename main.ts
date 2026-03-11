316b08e9ed5d19d6c3da5259ba714c7672689594
Betacarinae <finn.d.fagan@gmail.com>
2026-03-10 21:11:08 -0600

    Made it compile!

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-import { readFileSync } from 'node:fs'
+import { readFileSync, writeFileSync } from 'node:fs'
 import { argv } from 'node:process'
 import { inspect } from 'node:util'
+import { exit } from 'node:process'
+import * as ir from './ir'
 import * as ohm from 'ohm-js'
+import llvm from 'llvm-bindings'
 
 const code = readFileSync(argv[2], 'utf-8')
 const grammar = ohm.grammar(readFileSync('./cruton.ohm', 'utf-8'))
@@ -49,8 +52,45 @@ if(mr.failed()) {
     ast = semantics(mr).ast()
 }
 
+const utils = {
+    printHelper(text: string) {
+        const str = ir.createStringPTR(text)
+        ir.call(ir.printf, [str])
+    }
+}
+
 if(ast) {
     console.log('AST Build Success')
-    console.log(JSON.stringify(ast, null, 2))
-    
+    console.log(JSON.stringify(ast, null, 2)) 
+    if(argv[3] == '--ast') {
+        exit(0)
+    } 
+    parseAST(ast) 
+}
+
+function parseAST(ast: any[]) {
+    for(let section in ast) {
+        for(let line in ast[section].body) {
+            parse(ast[section].body[line])
+        }
+    }
+}
+
+function parse(astfrag: any) {
+    switch(astfrag.type) {
+        case 'print':
+            utils.printHelper(astfrag.value + '\n')
+            
+    }
+}
+
+
+ir.builder.CreateRet(ir.builder.getInt32(0))
+
+console.log(ir.module.print())
+try {
+    llvm.verifyModule(ir.module)
+    writeFileSync('out.ll', ir.module.print())
+} catch(err) {
+    console.log(err)
 }
